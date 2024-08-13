3b74d889e874a7553c8d6c15cf7ad552ee7e4fab
jameshawes <james.hawes@thevalueengineers.com>
2024-08-13 11:27:49 +0100

    move temp testing data to setup.R file

--- tests/testthat/test-generate_table.R
+++ tests/testthat/test-generate_table.R
@@ -1,59 +1,12 @@
-#temporary tables for testing
 
-#single code only
-temp_single_table <- generate_table(
-  testTableData,
-  row_vars = "agegroup",
-  col_var = "s2",
-  weight_var = "gender_weight1",
-  variable_labels = tveDataLoader::get_varLabels(testTableData),
-  value_labels = tveDataLoader::get_valLabels(testTableData)
-)
-
-#multi code only
-temp_multi_table <- generate_table(
-  testTableData,
-  row_vars = paste0("s1_", sprintf("%02d", c(01:08, 99))),
-  col_var = "s2",
-  weight_var = "gender_weight1",
-  variable_labels = tveDataLoader::get_varLabels(testTableData),
-  value_labels = tveDataLoader::get_valLabels(testTableData)
-)
-
-#numeric only
-temp_numeric_table <- generate_table(
-  testTableData,
-  row_vars = "s3",
-  col_var = "s2",
-  weight_var = "gender_weight1",
-  variable_labels = tveDataLoader::get_varLabels(testTableData),
-  value_labels = tveDataLoader::get_valLabels(testTableData)
-)
-
-#combined with single, multi and numeric
-temp_combined_table <- generate_table(
-  testTableData,
-  row_vars = c(paste0("s1_", sprintf("%02d", c(01:08, 99))),"s2", "s3"),
-  col_var = "s2",
-  weight_var = "gender_weight1",
-  variable_labels = tveDataLoader::get_varLabels(testTableData),
-  value_labels = tveDataLoader::get_valLabels(testTableData)
-)
-
-#unweighted combined table
-temp_combined_table_unweighted <- generate_table(
-  testTableData,
-  row_vars = c(paste0("s1_", sprintf("%02d", c(01:08, 99))),"s2", "s3"),
-  col_var = "s2",
-  weight_var = NULL,
-  variable_labels = tveDataLoader::get_varLabels(testTableData),
-  value_labels = tveDataLoader::get_valLabels(testTableData)
-)
 
 #test the sum of all columns equals 1 for single code questions
 test_that("sum of values in columns 4 to n is equal to the number of those columns", {
   # Calculate the sum of each column's values
-  column_sums <- purrr::map_dbl(temp_single_table[, 4:ncol(temp_single_table)], ~sum(.x, na.rm = TRUE))
+  column_sums <- purrr::map_dbl(
+    temp_single_table[, 4:ncol(temp_single_table)],
+    ~sum(.x, na.rm = TRUE)
+  )
 
   # Calculate the total sum of these columns
   total_sum <- sum(column_sums)
